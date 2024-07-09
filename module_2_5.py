69023bb925071cb7ea267c46f6b8ce71c1e99b62
cherepanovig <cherepanovig@gmail.com>
2024-07-10 05:25:14 +0700

    Update module_2_5.py

--- module_2_5.py
+++ module_2_5.py
@@ -4,11 +4,13 @@ def get_matrix(n, m, value): # Задаем функцию
         matrix.append([])  # Добавляем количество пустых списков в соотв-ии с 'n'
         for j in range(m):
             matrix[i].append(value)  # Добавляем 'value' в списки[i] в соотв-ии с 'm'
-    print(matrix)
+    #print(matrix)
+    return(matrix)
+
+
 result1 = get_matrix(2, 2, 10)
 result2 = get_matrix(3, 5, 42)
 result3 = get_matrix(4, 2, 13)
 print(result1)
 print(result2)
 print(result3)
-# Если закомментировать 3 последних строки, то None не будет выводиться,
